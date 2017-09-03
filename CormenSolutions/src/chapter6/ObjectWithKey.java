8ce9f2fe4034f77c9a76a35c01cc7d47c1356396
rajan <rajan44chauhan@gmail.com>
2017-09-03 15:15:53 +0530

    implemented quicksort algorithms with randomization and simple implementation.

--- CormenSolutions/src/chapter6/ObjectWithKey.java
+++ CormenSolutions/src/chapter6/ObjectWithKey.java
@@ -11,4 +11,26 @@ public class ObjectWithKey {
 		this.key = key;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + key;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ObjectWithKey other = (ObjectWithKey) obj;
+		if (key != other.key)
+			return false;
+		return true;
+	}
+
 }
--- CormenSolutions/src/chapter6/Tasks.java
+++ CormenSolutions/src/chapter6/Tasks.java
@@ -22,4 +22,29 @@ public class Tasks extends ObjectWithKey {
 		return "Tasks [description=" + description + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tasks other = (Tasks) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		return true;
+	}
+
 }
